5bef6c7dbef6a8ebc7084677bfb6ddcc34d8f138
Aces007 <acelawrencezclavano@iskolarngbayan.pup.edu.ph>
2022-02-08 22:14:51 -0800

    still experimenting

--- dummy.py
+++ dummy.py
@@ -1,28 +1,71 @@
+from ctypes import alignment
+import json
 from fpdf import FPDF
 
-# create FPDF object
-# Layout ('P','L')
-# Unit ('mm', 'cm', 'in')
-# format ('A3', 'A4' (default), 'A5', 'Letter', 'Legal', (100,150))
-pdf = FPDF('P', 'mm', 'Letter')
-
-# Add a page
+#PDF Format
+pdf = FPDF('P', 'cm', 'A4') 
 pdf.add_page()
 
-# specify font
-# fonts ('times', 'courier', 'helvetica', 'symbol', 'zpfdingbats')
-# 'B' (bold), 'U' (underline), 'I' (italics), '' (regular), combination (i.e., ('BU'))
-pdf.set_font('helvetica', 'BIU', 16)
-pdf.set_text_color(220,50,50)
-# Add text
-# w = width
-# h = height
-# txt = your text
-# ln (0 False; 1 True - move cursor down to next line)
-# border (0 False; 1 True - add border around cell)
-pdf.cell(120, 100, 'Hello World!', ln=True, border=True)
 
-pdf.set_font('times', '', 12)
-pdf.cell(80, 10, 'Good Bye World!')
+#Read Informations
+Curriculum_Vitae= open('isapang.json', 'r')
+Curri_Vitae= Curriculum_Vitae.read()
+Curriculum_Vitae.close()
 
-pdf.output('pdf_1.pdf')
+#Informations
+for BasicInfo in Curri_Vitae:
+    pdf.set_font('times', 'B', 40)
+    pdf.cell(18.5,1, f"{BasicInfo['Name']}", align= "C", ln=1)
+    pdf.set_font('times', 'I', 12)
+    pdf.cell(18.5,2, (BasicInfo['Age']), align= "C", ln=1)
+    pdf.set_font('times', "B", 15)
+    pdf.cell(5.25,1,"Personal Information", border= 1, ln=1)
+    pdf.set_font('helvetica', "", 10)
+    pdf.cell(8.5,1,"", )
+    pdf.cell(2,1, f"Permanent Address: {BasicInfo['Address']}",align="C", ln=1)
+    pdf.cell(8.5,1,"", )
+    pdf.cell(2,1, f"Email Address: {BasicInfo['Email']}",align="C", ln=1)
+    pdf.cell(8.5,1,"", )
+    pdf.cell(2,1, f"Birthdate: {BasicInfo['Birthdate']}",align="C", ln=1)
+    pdf.cell(8.5,1,"", )
+    pdf.cell(2,1, f"Gender: {BasicInfo['Sex']}",align="C", ln=1)
+    pdf.cell(8.5,1,"", )
+    pdf.cell(2,1, f"Hobbies: {BasicInfo['Hobbies']}", align="C", ln=1)
+    pdf.cell(8.5,1,"", )
+    pdf.cell(2,1, f"Civil Status: {BasicInfo['Civil Status']}", align="C", ln=1)
+    pdf.set_font('times', "B", 15)
+   
+    pdf.cell(1.75,1,"Skills", border=1, ln=1)
+    pdf.set_font('helvetica', "", 10)
+    # for skills in Information_User ['skills']:
+    #     pdf.cell(8.5,1,"", )
+    #     pdf.cell(2,1, (skills), align="C", ln=1)
+    # pdf.set_font('times', "B", 15)
+   
+    # pdf.cell(3.5,1,"Achievements", border=1, ln=1)
+    # pdf.set_font('helvetica', "", 10)
+    # for achievements in Information_User ['achievements']:
+    #     pdf.cell(8.5,1,"", )
+    #     pdf.cell(2,1, (achievements), align="C", ln=1)
+    # pdf.set_font('times', "B", 15)
+    # pdf.cell(6,1,"Educational Background", border=1, ln=1)
+    # pdf.set_font('helvetica', "", 10)
+    # for educationalbg in Information_User ['educationalbg']:
+    #     pdf.cell(8.5,1,"", )
+    #     pdf.cell(2,1, (educationalbg['School']), align="C", ln=1) 
+    #     pdf.cell(8.5,1,"",)
+    #     pdf.cell(2,1, (educationalbg['AcadYear']), align="C", ln=1) 
+
+#PDF Maker
+pdf.output('Murallos_SOLANA')
+
+
+
+# # Calling font size for the header
+# def Headerhehe():
+#         Resume.set_font('times', 'B', 20)
+#         Resume.cell(200, 25, txt = "Curriculum Vitae", 
+#                 ln = 1, align = 'C')
+#         Resume.set_font('times', '', 16)        
+#         Resume.cell(200, 1, txt = "Polytechnic University of the Philippines", 
+#                 align = 'C')
--- open.py
+++ open.py
@@ -1,23 +1,48 @@
+# Imports
 import json
 from fpdf import FPDF
+from ctypes import alignment
 
-with open ('isapang.json', 'r') as f:
-    Ayt = f.read()
-    json.loads(Ayt)
-
-
+# Calling PDF Generator, Rules, etc.
 Resume = FPDF('P', 'mm', 'Letter')
-
 Resume.set_auto_page_break(auto = True, margin = 15)
-
 Resume.add_page()
 
-Resume.set_font('times', '', 18)
+# Reading
+Curriculum_Vitae = open('isapang.json', 'r')
+Curri_Vitae = json.load(Curriculum_Vitae)
+Curriculum_Vitae.close()
 
-Resume.cell(200, 25, txt = "Curriculum Vitae", 
-        ln = 1, align = 'C')
-Resume.cell(200, 1, txt = "Polytechnic University of the Philippines", 
-        align = 'C')
 
+
+# Content section
+for BasicInfo in Curri_Vitae:
+        Resume.set_font('times', 'B', 15)
+        Resume.cell(77,5, "Personal Information", align= 'C', ln=1)
+        Resume.set_font('times', '', 12)
+        Resume.cell(86.5,10, f"Name: {BasicInfo['Name']}", align= 'C', ln=1)
+        Resume.set_font('times', '', 12)
+        Resume.cell(43.5,4, f"Age: {BasicInfo['Age']}", align= 'C', ln=1)
+        Resume.set_font('times', '', 12)
+        Resume.cell(88,12, f"Email: {BasicInfo['Email']}", align= 'C', ln=1)
+        Resume.set_font('times', '', 12)
+        Resume.cell(114.5,5, f"Permanent Address: {BasicInfo['Permanent Address']}", align= 'C', ln=1)
+        Resume.set_font('times', '', 12)
+        Resume.cell(63.5,13, f"Civil Status: {BasicInfo['Civil Status']}", align= 'C', ln=1)
+        Resume.set_font('times', '', 12)
+        Resume.cell(138.5,6, f"Hobbies: {BasicInfo['Hobbies']}", align= 'C', ln=1)
+        Resume.cell (30)
+        Resume.set_font('times', 'B', 15)
+        Resume.cell(27,25, "Educational Background", align= 'C', ln=1)
+        for EducBG in BasicInfo ['Education']:
+                Resume.set_font('times', '', 12, )
+                Resume.cell(140.5,9, f"Institution: {EducBG['Institution']}", align= 'C', ln=1)
+                Resume.set_font('times', '', 12, )
+                Resume.cell(165.5,9, f"School Year: {EducBG['A.Y']}", align= 'C', ln=1)
+        
+
+# PDF Name Format
 Resume.output('CLAVANO, ACE LAWRENCE.pdf')
+
+
 
